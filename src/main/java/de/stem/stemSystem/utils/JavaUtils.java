b1a65cfb2fc3bfb33f59b7efdff8a11a10a30219
Niklas <niklas.linz@enigmar.de>
2022-10-15 19:37:20 +0200

    move to openjl

--- src/main/java/de/stem/stemSystem/utils/JavaUtils.java
+++ src/main/java/de/stem/stemSystem/utils/JavaUtils.java
@@ -44,35 +44,4 @@ public class JavaUtils {
         return version;
     }
 
-    public static double getSystemLoad() {
-        OperatingSystemMXBean operatingSystemMXBean = ManagementFactory.getOperatingSystemMXBean();
-        return operatingSystemMXBean.getSystemLoadAverage();
-    }
-
-    public static int getCoreAmount() {
-        OperatingSystemMXBean operatingSystemMXBean = ManagementFactory.getOperatingSystemMXBean();
-        return operatingSystemMXBean.getAvailableProcessors();
-    }
-
-    public static double getUsedMemory() {
-        MemoryMXBean memoryMXBean = ManagementFactory.getMemoryMXBean();
-        return (memoryMXBean.getHeapMemoryUsage().getUsed() / (1000 * 1000));
-    }
-
-    public static double getMaxMemory() {
-        MemoryMXBean memoryMXBean = ManagementFactory.getMemoryMXBean();
-        return (memoryMXBean.getHeapMemoryUsage().getMax() / (1000 * 1000));
-    }
-
-    public static long getUsableSpace() {
-        return new File("/").getUsableSpace();
-    }
-
-    public static long getTotalSpace() {
-        return new File("/").getTotalSpace();
-    }
-
-    public static Instant getTimeInstant() {
-        return Instant.now(Clock.systemDefaultZone());
-    }
 }
