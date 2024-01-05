d2f82edab05635506ff17b06ec89b7104383497e
Niklas <niklas.linz@enigmar.de>
2024-01-05 19:12:06 +0100

    rename version

--- src/main/java/de/stem/stemSystem/utils/JavaUtils.java
+++ src/main/java/de/stem/stemSystem/utils/JavaUtils.java
@@ -45,10 +45,11 @@ public class JavaUtils {
         InputStream inStream = Thread.currentThread().getContextClassLoader().getResourceAsStream("version.yml");
         Yaml yaml = new Yaml();
         Map<String, Object> obj = yaml.load(inStream);
+        String kernelName = String.valueOf(obj.get("kernelName"));
         String versionNumber = String.valueOf(obj.get("buildVersion"));
         String buildNumber = String.valueOf(obj.get("buildNumber"));
         String buildLabel = String.valueOf(obj.get("buildLabel"));
-        return versionNumber + "." + buildNumber + "-" + buildLabel;
+        return kernelName.toUpperCase() + "_" + versionNumber + "." + buildNumber + "-" + buildLabel;
     }
 
 }
