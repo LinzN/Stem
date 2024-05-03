51e6951754ce8307883c8d1f7a709c0a52ae4d3f
Niklas <niklas.linz@enigmar.de>
2024-05-03 12:31:48 +0200

    extend updater

--- src/main/java/de/stem/stemSystem/modules/pluginModule/UpdateCheck.java
+++ src/main/java/de/stem/stemSystem/modules/pluginModule/UpdateCheck.java
@@ -23,27 +23,62 @@ public class UpdateCheck {
         if (stemBuildId.equalsIgnoreCase("IDEA")) {
             STEMSystemApp.LOGGER.WARNING("STEM Framework is using a custom build! No update check available!");
         } else {
-            int buildId = Integer.parseInt(stemBuildId);
-            int newestBuildId = this.getNewestJobId("stem");
-            if (buildId < newestBuildId) {
-                STEMSystemApp.LOGGER.CONFIG("A new build is available for STEM Framework. Current #" + buildId + " newest #" + newestBuildId);
+            int currentId = Integer.parseInt(stemBuildId);
+            int newestId = this.getNewestJobId("stem");
+
+            String newestBuiltRevision = getBuiltRevision("stem", newestId);
+            String currentBuiltRevision = getBuiltRevision("stem", currentId);
+
+            if (!newestBuiltRevision.equalsIgnoreCase(currentBuiltRevision)) {
+                STEMSystemApp.LOGGER.CONFIG("A new build is available for STEM Framework. Current #" + currentId + " SHA " + currentBuiltRevision + " newest #" + newestId + " SHA " + newestBuiltRevision);
             }
         }
 
         for (STEMPlugin stemPlugin : this.pluginModule.getLoadedPlugins()) {
+            STEMSystemApp.LOGGER.INFO("Checking updates for plugin " + stemPlugin.getPluginName());
             String buildID = stemPlugin.getBuildNumber();
             String jobName = stemPlugin.getBuildJobName();
             if (jobName.equalsIgnoreCase("CUSTOM") || buildID.equalsIgnoreCase("SNAPSHOT")) {
                 STEMSystemApp.LOGGER.WARNING("The current build #" + buildID + " for plugin " + stemPlugin.getPluginName() + " is a custom build. No update check available.");
             } else {
                 int newestId = this.getNewestJobId(jobName);
-                int jobId = Integer.parseInt(buildID);
-                if (jobId < newestId) {
-                    STEMSystemApp.LOGGER.CONFIG("The current build #" + jobId + " for plugin " + stemPlugin.getPluginName() + " is outdated. There is a new build #" + newestId + " available");
+                int currentId = Integer.parseInt(buildID);
+
+                String newestBuiltRevision = getBuiltRevision(jobName, newestId);
+                String currentBuiltRevision = getBuiltRevision(jobName, currentId);
+
+
+                if (!newestBuiltRevision.equalsIgnoreCase(currentBuiltRevision)) {
+                    STEMSystemApp.LOGGER.CONFIG("The current build #" + currentId + " with SHA " + currentBuiltRevision + " for plugin " + stemPlugin.getPluginName() + " is outdated. There is a new build #" + newestId + " with SHA " + newestBuiltRevision + " available.");
                 } else {
-                    STEMSystemApp.LOGGER.INFO("The current build #" + jobId + " for plugin " + stemPlugin.getPluginName() + " is up to date.");
+                    STEMSystemApp.LOGGER.INFO("The current build #" + currentId + " for plugin " + stemPlugin.getPluginName() + " is up to date.");
                 }
             }
+        }
+    }
+
+    private String getBuiltRevision(String jobName, int jobId) {
+        JSONObject jsonObject = null;
+        try {
+            URL url = new URL(this.pluginModule.jenkinsURL + "/job/" + jobName + "/" + jobId + "/api/json?pretty=true");
+            InputStream input = url.openStream();
+            InputStreamReader isr = new InputStreamReader(input);
+            BufferedReader reader = new BufferedReader(isr);
+            StringBuilder json = new StringBuilder();
+            int c;
+            while ((c = reader.read()) != -1) {
+                json.append((char) c);
+            }
+            jsonObject = new JSONObject(json.toString());
+            JSONObject revisionTab = jsonObject.getJSONArray("actions").getJSONObject(2);
+
+            if (jsonObject.getJSONArray("actions").getJSONObject(2).isEmpty()) {
+                revisionTab = jsonObject.getJSONArray("actions").getJSONObject(1);
+            }
+            return revisionTab.getJSONObject("lastBuiltRevision").getString("SHA1");
+        } catch (Exception e) {
+            STEMSystemApp.LOGGER.ERROR(e);
+            return "error";
         }
     }
 
